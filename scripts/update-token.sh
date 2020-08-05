932863eb38da1e43a729cf47f33e07c64527ec90
Alex Boten <aboten@lightstep.com>
2020-08-05 12:21:40 -0700

    chore: adding script to update token for CI

---
+++ scripts/update-token.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TOKEN=${{ secrets.ACCESS_TOKEN }}
+
+cp example.env .env
+sed -i '' "s/<ACCESS TOKEN>/${TOKEN}/" .env
+cp example-collector-config.yaml ./collector/collector-config.yaml
+sed -i '' "s/<ACCESS TOKEN>/${TOKEN}/" ./collector/collector-config.yaml
