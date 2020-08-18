b18226865b3e2f1351d430571d9c110d6134585f
Alex Boten <aboten@lightstep.com>
2020-08-18 17:30:48 -0500

    update client to use env variables

--- all/client.py
+++ all/client.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
 import time
 
-from opentelemetry import trace
 import requests
+from environs import Env
+from opentelemetry import trace
 
 tracer = trace.get_tracer(__name__)
 
 
-def send_requests():
-    destinations = [
-        "http://go-opentracing-server:8081/content/456",
-        "http://go-otlp-server:8081/content/345",
-        "http://go-launcher-server:8081/content/234",
-        "http://py-lstrace-server:5000/redis/124",
-        "http://py-collector-server:5000/sqlalchemy/123",
-        "http://py-otlp-server:5000/pymongo/123",
-        "http://py-launcher-server:5000/redis/123",
-        "http://js-lstrace-server:8080/ping",
-        "http://js-launcher-server:8080/ping",
-        "http://java-specialagent-server:8083/content",
-        "http://java-otlp-server:8083/content",
-    ]
+def send_requests(destinations):
     with tracer.start_as_current_span("send_requests"):
         for url in destinations:
             try:
@@ -31,6 +19,9 @@ def send_requests():
 
 
 if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    destinations = env.list("DESTINATIONS")
     while True:
-        send_requests()
+        send_requests(destinations)
         time.sleep(2)
