f685d2180cf424fcec5571994ac13ae87a0fa966
Alex Boten <aboten@lightstep.com>
2020-10-08 15:04:21 -0700

    one more fix to precommit script

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -2,7 +2,7 @@
 
 docker-compose up config-generator
 
-if [ -z $(git diff --quiet --exit-code) ]; then 
+if [ -z $(git diff --quiet --exit-code ./integration) ]; then 
     echo "config has changed"
     git status
     exit 1
