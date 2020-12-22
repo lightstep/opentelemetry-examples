82a0bdf238d186eb54147545e4e682e4031d826e
Alex Boten <aboten@lightstep.com>
2020-12-22 09:39:49 -0800

    fix issues w/ python otel examples

--- python/opentelemetry/common.py
+++ python/opentelemetry/common.py
@@ -5,7 +5,6 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
 from opentelemetry.propagators import set_global_textmap
 from opentelemetry.propagators.composite import CompositeHTTPPropagator
-from opentelemetry.sdk.trace import Resource
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.sdk.trace.propagation.b3_format import B3Format
 
@@ -30,12 +29,6 @@ def get_otel_tracer():
 
     trace.get_tracer_provider().add_span_processor(
         BatchExportSpanProcessor(span_exporter)
-    )
-    trace.get_tracer_provider().resource = Resource(
-        {
-            "service.name": os.getenv("LS_SERVICE_NAME"),
-            "service.version": os.getenv("LS_SERVICE_VERSION"),
-        }
     )
     return trace.get_tracer(__name__)
 
