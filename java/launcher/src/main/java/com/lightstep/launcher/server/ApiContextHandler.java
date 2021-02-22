f9b402a43b4f744599bdcae511e4f8bc0df5fb10
Sergei Malafeev <sergei@malafeev.org>
2021-02-22 20:47:54 +0800

    update java launcher to 0.17.0
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

--- java/launcher/src/main/java/com/lightstep/launcher/server/ApiContextHandler.java
+++ java/launcher/src/main/java/com/lightstep/launcher/server/ApiContextHandler.java
@@ -1,12 +1,16 @@
 package com.lightstep.launcher.server;
 
-import io.opentelemetry.OpenTelemetry;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.trace.Span;
-import io.opentelemetry.trace.Tracer;
-import io.opentelemetry.trace.TracingContextUtils;
+import io.opentelemetry.context.propagation.TextMapGetter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,35 +32,47 @@ public class ApiContextHandler extends ServletContextHandler {
     public void doGet(HttpServletRequest req, HttpServletResponse res)
         throws ServletException, IOException {
 
-      io.grpc.Context context = OpenTelemetry.getPropagators().getTextMapPropagator().extract(
-          io.grpc.Context.current(), req, HttpServletRequest::getHeader);
+      io.opentelemetry.context.Context parentContext = GlobalOpenTelemetry.getPropagators()
+          .getTextMapPropagator().extract(
+              io.opentelemetry.context.Context.current(), req,
+              new TextMapGetter<HttpServletRequest>() {
+                @Override
+                public Iterable<String> keys(HttpServletRequest carrier) {
+                  final Enumeration<String> headerNames = carrier.getHeaderNames();
+                  List<String> keys = new ArrayList<>();
+                  while (headerNames.hasMoreElements()) {
+                    keys.add(headerNames.nextElement());
+                  }
+                  return keys;
+                }
 
-      Span.Builder builder = tracer
-          .spanBuilder("/");
-      Span parent = TracingContextUtils.getSpanWithoutDefault(context);
-      if (parent != null) {
-        builder.setParent(parent);
-      }
-      Span span = builder.startSpan();
+                @Override
+                public String get(HttpServletRequest carrier, String key) {
+                  return carrier.getHeader(key);
+                }
+              });
 
-      try (Scope scope = tracer.withSpan(span)) {
-        // Set the Semantic Convention
-        span.setAttribute("component", "http");
-        span.setAttribute("http.method", "GET");
-        span.setAttribute("http.scheme", "http");
-        span.setAttribute("http.target", "/");
-        try (PrintWriter writer = res.getWriter()) {
-          writer.write(createRandomString());
+      try (final Scope scope = parentContext.makeCurrent()) {
+        Span span = tracer.spanBuilder("/").startSpan();
+        try {
+          span.setAttribute("component", "http");
+          span.setAttribute("http.method", "GET");
+          span.setAttribute("http.scheme", "http");
+          span.setAttribute("http.target", "/");
+          try (PrintWriter writer = res.getWriter()) {
+            writer.write(createRandomString());
+          }
+        } finally {
+          // Close the span
+          span.end();
         }
-      } finally {
-        // Close the span
-        span.end();
       }
+
     }
 
     // OTel API
     private static Tracer tracer =
-        OpenTelemetry.getTracer("io.opentelemetry.example.http.HttpServer");
+        GlobalOpenTelemetry.getTracer("io.opentelemetry.example.http.HttpServer");
 
     String createRandomString() {
       int length = rand.nextInt(1023) + 1;
