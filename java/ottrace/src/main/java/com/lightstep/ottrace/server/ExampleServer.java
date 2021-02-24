575fecf702e6aced15caf274a0436bea1ace1d35
Sergei Malafeev <sergei@malafeev.org>
2021-02-24 20:16:24 +0800

    add java ottrace client/server
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

---
+++ java/ottrace/src/main/java/com/lightstep/ottrace/server/ExampleServer.java
@@ -0,0 +1,49 @@
+package com.lightstep.ottrace.server;
+
+import com.lightstep.opentelemetry.launcher.OpenTelemetryConfiguration;
+import com.lightstep.opentelemetry.launcher.Propagator;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+
+public class ExampleServer {
+
+  public static void main(String[] args) throws Exception {
+    OpenTelemetryConfiguration.newBuilder()
+        .setPropagator(Propagator.OT_TRACE)
+        .install();
+
+    Tracer tracer = GlobalOpenTelemetry.getTracer("LightstepExample");
+
+    Span span = tracer.spanBuilder("start example").setSpanKind(SpanKind.CLIENT).startSpan();
+    span.setAttribute("Attribute 1", "Value 1");
+    span.addEvent("Event 0");
+    // execute my use case - here we simulate a wait
+    doWork();
+    span.addEvent("Event 1");
+    span.end();
+
+    ContextHandlerCollection handlers = new ContextHandlerCollection();
+    handlers.setHandlers(new Handler[]{
+        new ApiContextHandler(),
+    });
+    Server server = new Server(8084);
+    server.setHandler(handlers);
+
+    server.start();
+    server.dumpStdErr();
+    server.join();
+  }
+
+  private static void doWork() {
+    try {
+      TimeUnit.SECONDS.sleep(1);
+    } catch (InterruptedException ignore) {
+    }
+  }
+}
