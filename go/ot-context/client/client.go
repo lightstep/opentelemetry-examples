2ddb637650be77e87f913732b3a511e124c47344
Alex Boten <aboten@lightstep.com>
2021-05-04 11:57:23 -0700

    update example

--- go/ot-context/client/client.go
+++ go/ot-context/client/client.go
@@ -1,5 +1,5 @@
 //
-// example code to test lightstep/opentelemetry-exporter-go
+// example code to test lightstep/otel-launcher-go/launcher
 //
 // usage:
 //   LS_ACCESS_TOKEN=${SECRET_TOKEN} \
@@ -12,92 +12,18 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/lightstep/otel-launcher-go/launcher"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/contrib/propagators/ot"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
-	"go.opentelemetry.io/otel/label"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	"google.golang.org/grpc/credentials"
 )
 
 var (
-	componentName  = os.Getenv("LS_SERVICE_NAME")
-	serviceVersion = os.Getenv("LS_SERVICE_VERSION")
-	lsToken        = os.Getenv("LS_ACCESS_TOKEN")
-	collectorURL   = os.Getenv("LS_SATELLITE_URL")
-	targetURL      = os.Getenv("DESTINATION_URL")
-	insecure       = os.Getenv("LS_INSECURE")
+	destinationURL = os.Getenv("DESTINATION_URL")
 )
-
-func initExporter(url string, token string) *otlp.Exporter {
-	headers := map[string]string{
-		"lightstep-access-token": token,
-	}
-
-	secureOption := otlpgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
-	if len(insecure) > 0 {
-		secureOption = otlpgrpc.WithInsecure()
-	}
-
-	exporter, err := otlp.NewExporter(
-		context.Background(),
-		otlpgrpc.NewDriver(
-			secureOption,
-			otlpgrpc.WithEndpoint(url),
-			otlpgrpc.WithHeaders(headers),
-		),
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return exporter
-}
-
-func initTracer() {
-	otPropagator := ot.OT{}
-	// Register the OT propagator globally.
-	otel.SetTextMapPropagator(otPropagator)
-	if len(collectorURL) == 0 {
-		collectorURL = "localhost:55680"
-	}
-
-	if len(componentName) == 0 {
-		componentName = "test-go-client"
-	}
-	if len(serviceVersion) == 0 {
-		serviceVersion = "0.0.0"
-	}
-
-	exporter := initExporter(collectorURL, lsToken)
-
-	resources, err := resource.New(
-		context.Background(),
-		resource.WithAttributes(
-			label.String("service.name", componentName),
-			label.String("service.version", serviceVersion),
-			label.String("library.language", "go"),
-			label.String("library.version", "1.2.3"),
-		),
-	)
-	if err != nil {
-		log.Printf("Could not set resources: ", err)
-	}
-	tp := trace.NewTracerProvider(
-		trace.WithConfig(trace.Config{DefaultSampler: trace.AlwaysSample()}),
-		trace.WithSyncer(exporter),
-		trace.WithResource(resources),
-	)
-	otel.SetTracerProvider(tp)
-}
 
 func makeRequest() {
 	httpClient := &http.Client{
@@ -106,21 +32,24 @@ func makeRequest() {
 	tracer := otel.Tracer("otel-example/client")
 	_, span := tracer.Start(context.Background(), "makeRequest")
 	defer span.End()
+	req, _ := http.NewRequest("GET", destinationURL, nil)
 
-	req, _ := http.NewRequest("GET", targetURL, nil)
 	res, err := httpClient.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer res.Body.Close()
-	fmt.Printf("Request to %s, got %d bytes\n", targetURL, res.ContentLength)
+	fmt.Printf("Request to %s, got %d bytes\n", destinationURL, res.ContentLength)
 }
 
 func main() {
-	initTracer()
-	if len(targetURL) == 0 {
-		targetURL = "http://localhost:8081"
+	otel := launcher.ConfigureOpentelemetry(
+		launcher.WithPropagators([]string{"ottrace"}),
+	)
+	defer otel.Shutdown()
+	if len(destinationURL) == 0 {
+		destinationURL = "http://localhost:8081"
 	}
 	for {
 		makeRequest()
--- go/ot-context/client/go.mod
+++ go/ot-context/client/go.mod
@@ -3,10 +3,7 @@ module github.com/lightstep/ls-examples/go/opentelemetry/client
 go 1.14
 
 require (
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.17.0
-	go.opentelemetry.io/contrib/propagators v0.17.0
-	go.opentelemetry.io/otel v0.17.0
-	go.opentelemetry.io/otel/exporters/otlp v0.17.0
-	go.opentelemetry.io/otel/sdk v0.17.0
-	google.golang.org/grpc v1.35.0
+	github.com/lightstep/otel-launcher-go v0.18.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.18.0
+	go.opentelemetry.io/otel v0.18.0
 )
