7c9c81646fcba2f8a26702d8408d683ba804a95e
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2022-06-06 16:32:18 -0500

    simplify example

--- collector/elasticsearch/setup/setup-keystore.sh
+++ collector/elasticsearch/setup/setup-keystore.sh
@@ -6,7 +6,6 @@ OUTPUT_KEYSTORE=/secrets/keystore/elasticsearch.keystore
 
 GENERATED_SERVICE_TOKENS=/usr/share/elasticsearch/config/service_tokens
 OUTPUT_SERVICE_TOKENS=/secrets/service_tokens
-OUTPUT_KIBANA_TOKEN=/secrets/.env.kibana.token
 
 # Password Generate
 PW=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16 ;)
@@ -21,17 +20,6 @@ elasticsearch-keystore create >> /dev/null
 # Setting Secrets and Bootstrap Password
 sh /setup/keystore.sh
 echo "Elastic Bootstrap Password is: $ELASTIC_PASSWORD"
-
-# Generating Kibana Token
-echo "Generating Kibana Service Token..."
-
-# Delete old token if exists
-/usr/share/elasticsearch/bin/elasticsearch-service-tokens delete elastic/kibana default &> /dev/null || true
-
-# Generate new token
-TOKEN=$(/usr/share/elasticsearch/bin/elasticsearch-service-tokens create elastic/kibana default | cut -d '=' -f2 | tr -d ' ')
-echo "Kibana Service Token is: $TOKEN"
-echo "KIBANA_SERVICE_ACCOUNT_TOKEN=$TOKEN" > $OUTPUT_KIBANA_TOKEN
 
 # Replace current Keystore
 if [ -f "$OUTPUT_KEYSTORE" ]; then
@@ -60,5 +48,4 @@ printf "Remember to restart the stack, or reload secure settings if changed sett
 printf "About Reloading Settings: https://www.elastic.co/guide/en/elasticsearch/reference/current/secure-settings.html#reloadable-secure-settings\n"
 printf "=====================================================\n"
 printf "Your 'elastic' user password is: $ELASTIC_PASSWORD\n"
-printf "Your Kibana Service Token is: $TOKEN\n"
 printf "=====================================================\n"
