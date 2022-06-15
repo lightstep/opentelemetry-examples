345633edd4ebe981f7f399195cd89edb92296c46
Adriana Villela <indycomp0t1@gmail.com>
2022-06-15 16:37:56 -0400

    Update client with newer Go OTel SDK

--- go/opentelemetry/client/client.go
+++ go/opentelemetry/client/client.go
@@ -12,92 +12,198 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	// "log"
+	"io/ioutil"
 	"net/http"
-	"os"
+	// "os"
 	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/contrib/propagators/b3"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
+	// "go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	// "go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	"google.golang.org/grpc/credentials"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	"go.opentelemetry.io/otel/trace"
+	// "go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	// "go.opentelemetry.io/contrib/propagators/b3"
+	// "go.opentelemetry.io/otel"
+	// "go.opentelemetry.io/otel/attribute"
+	// "go.opentelemetry.io/otel/exporters/otlp"
+	// "go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
+	// "go.opentelemetry.io/otel/propagation"
+	// "go.opentelemetry.io/otel/sdk/resource"
+	// sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	// semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	// "google.golang.org/grpc/credentials"
 )
+
+// var (
+// 	componentName  = os.Getenv("LS_SERVICE_NAME")
+// 	serviceVersion = os.Getenv("LS_SERVICE_VERSION")
+// 	// lsToken        = os.Getenv("LS_ACCESS_TOKEN")
+// 	collectorURL = os.Getenv("OTEL_COLLECTOR_URL")
+// 	targetURL    = os.Getenv("DESTINATION_URL")
+// 	// insecure       = os.Getenv("LS_INSECURE")
+// )
 
 var (
-	componentName  = os.Getenv("LS_SERVICE_NAME")
-	serviceVersion = os.Getenv("LS_SERVICE_VERSION")
-	lsToken        = os.Getenv("LS_ACCESS_TOKEN")
-	collectorURL   = os.Getenv("LS_SATELLITE_URL")
-	targetURL      = os.Getenv("DESTINATION_URL")
-	insecure       = os.Getenv("LS_INSECURE")
+	tracer         trace.Tracer
+	serviceName    string = "test-go-client"
+	serviceVersion string = "0.1.0"
+	collectorAddr  string = "localhost:4318" // HTTP endpoint for collector
+	targetURL      string = "http://localhost:8081/ping"
 )
 
-func initExporter(url string, token string) *otlp.Exporter {
-	headers := map[string]string{
-		"lightstep-access-token": token,
-	}
-
-	secureOption := otlpgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
-	if len(insecure) > 0 {
-		secureOption = otlpgrpc.WithInsecure()
-	}
-
-	exporter, err := otlp.NewExporter(
-		context.Background(),
-		otlpgrpc.NewDriver(
-			secureOption,
-			otlpgrpc.WithEndpoint(url),
-			otlpgrpc.WithHeaders(headers),
-		),
-	)
+func newTraceProvider(ctx context.Context) *sdktrace.TracerProvider {
+	exporter, err :=
+		otlptracehttp.New(ctx,
+			// WithInsecure lets us use http instead of https.
+			// This is just for local development.
+			otlptracehttp.WithInsecure(),
+			otlptracehttp.WithEndpoint(collectorAddr),
+		)
 
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	return exporter
+
+	// This includes the following resources:
+	//
+	// - sdk.language, sdk.version
+	// - service.name, service.version, environment
+	//
+	// Including these resources is a good practice because it is commonly
+	// used by various tracing backends to let you more accurately
+	// analyze your telemetry data.
+	resource, rErr :=
+		resource.Merge(
+			resource.Default(),
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(serviceName),
+				semconv.ServiceVersionKey.String(serviceVersion),
+				attribute.String("environment", "getting-started"),
+			),
+		)
+
+	if rErr != nil {
+		panic(rErr)
+	}
+
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource),
+	)
 }
 
-func initTracer() {
-	b3 := b3.B3{}
-	// Register the B3 propagator globally.
-	otel.SetTextMapPropagator(b3)
-	if len(collectorURL) == 0 {
-		collectorURL = "localhost:4317"
-	}
+// func initExporter(url string, token string) *otlp.Exporter {
+// 	headers := map[string]string{
+// 		"lightstep-access-token": token,
+// 	}
 
-	if len(componentName) == 0 {
-		componentName = "test-go-client"
-	}
-	if len(serviceVersion) == 0 {
-		serviceVersion = "0.0.0"
-	}
+// 	secureOption := otlpgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+// 	if len(insecure) > 0 {
+// 		secureOption = otlpgrpc.WithInsecure()
+// 	}
 
-	exporter := initExporter(collectorURL, lsToken)
+// 	exporter, err := otlp.NewExporter(
+// 		context.Background(),
+// 		otlpgrpc.NewDriver(
+// 			secureOption,
+// 			otlpgrpc.WithEndpoint(url),
+// 			otlpgrpc.WithHeaders(headers),
+// 		),
+// 	)
 
-	resources, err := resource.New(
-		context.Background(),
-		resource.WithAttributes(
-			attribute.String("service.name", componentName),
-			attribute.String("service.version", serviceVersion),
-			attribute.String("library.language", "go"),
-			attribute.String("library.version", "1.2.3"),
-		),
-	)
-	if err != nil {
-		log.Printf("Could not set resources: ", err)
-	}
-	tp := trace.NewTracerProvider(
-		trace.WithConfig(trace.Config{DefaultSampler: trace.AlwaysSample()}),
-		trace.WithSyncer(exporter),
-		trace.WithResource(resources),
-	)
-	otel.SetTracerProvider(tp)
-}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return exporter
+// }
+
+// func initTracer() {
+
+// 	if len(collectorURL) == 0 {
+// 		collectorURL = "localhost:4317"
+// 	}
+
+// 	if len(componentName) == 0 {
+// 		componentName = "test-go-client"
+// 	}
+// 	if len(serviceVersion) == 0 {
+// 		serviceVersion = "0.0.0"
+// 	}
+
+// 	ctx := context.Background()
+// 	res, err := resource.New(ctx,
+// 		resource.WithAttributes(
+// 			// Service name to be use by observability tool
+// 			semconv.ServiceNameKey.String(componentName)))
+// 	// Checking for errors
+// 	if err != nil {
+// 		fmt.Printf("Error adding %v to the tracer engine: %v", "applicationName", err)
+// 	}
+
+// 	traceExporter, err := otlptracegrpc.New(ctx,
+// 		otlptracegrpc.WithInsecure(),
+// 		otlptracegrpc.WithEndpoint(collectorURL),
+// 	)
+
+// 	// Check for errors
+// 	if err != nil {
+// 		fmt.Printf("Error initializing the trace exporter: %v", err)
+// 	}
+
+// 	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+// 	tp := sdktrace.NewTracerProvider(
+// 		sdktrace.WithResource(res),
+// 		sdktrace.WithSpanProcessor(bsp),
+// 	)
+// 	defer tp.Shutdown(context.Background())
+// 	otel.SetTracerProvider(tp)
+// 	otel.SetTextMapPropagator(propagation.TraceContext{})
+// }
+
+// func initTracerOld() {
+// 	b3 := b3.B3{}
+// 	// Register the B3 propagator globally.
+// 	otel.SetTextMapPropagator(b3)
+// 	if len(collectorURL) == 0 {
+// 		collectorURL = "localhost:4317"
+// 	}
+
+// 	if len(componentName) == 0 {
+// 		componentName = "test-go-client"
+// 	}
+// 	if len(serviceVersion) == 0 {
+// 		serviceVersion = "0.0.0"
+// 	}
+
+// 	exporter := initExporter(collectorURL, lsToken)
+
+// 	resources, err := resource.New(
+// 		context.Background(),
+// 		resource.WithAttributes(
+// 			attribute.String("service.name", componentName),
+// 			attribute.String("service.version", serviceVersion),
+// 			attribute.String("library.language", "go"),
+// 			attribute.String("library.version", "1.2.3"),
+// 		),
+// 	)
+// 	if err != nil {
+// 		log.Printf("Could not set resources: ", err)
+// 	}
+// 	tp := trace.NewTracerProvider(
+// 		trace.WithConfig(trace.Config{DefaultSampler: trace.AlwaysSample()}),
+// 		trace.WithSyncer(exporter),
+// 		trace.WithResource(resources),
+// 	)
+// 	otel.SetTracerProvider(tp)
+// }
 
 func makeRequest() {
 	httpClient := &http.Client{
@@ -114,14 +220,24 @@ func makeRequest() {
 		return
 	}
 	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Printf("Body : %s", body)
 	fmt.Printf("Request to %s, got %d bytes\n", targetURL, res.ContentLength)
 }
 
 func main() {
-	initTracer()
-	if len(targetURL) == 0 {
-		targetURL = "http://localhost:8081"
-	}
+	// initTracer()
+
+	ctx := context.Background()
+
+	tp := newTraceProvider(ctx)
+	defer func() { _ = tp.Shutdown(ctx) }()
+
+	otel.SetTracerProvider(tp)
+
+	// if len(targetURL) == 0 {
+	// 	targetURL = "http://localhost:8081/ping"
+	// }
 	for {
 		makeRequest()
 		time.Sleep(1 * time.Second)
--- go/opentelemetry/client/go.mod
+++ go/opentelemetry/client/go.mod
@@ -1,27 +1,30 @@
-module github.com/lightstep/ls-examples/go/opentelemetry/client
+module github.com/lightstep/opentelemetry-examples/go/opentelemetry/client
 
-go 1.17
+go 1.18
 
 require (
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.18.0
-	go.opentelemetry.io/contrib/propagators v0.18.0
-	go.opentelemetry.io/otel v0.18.0
-	go.opentelemetry.io/otel/exporters/otlp v0.18.0
-	go.opentelemetry.io/otel/sdk v0.18.0
-	google.golang.org/grpc v1.36.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.0
+	go.opentelemetry.io/otel v1.7.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.7.0
+	go.opentelemetry.io/otel/sdk v1.7.0
 )
 
 require (
-	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	go.opentelemetry.io/contrib v0.18.0 // indirect
-	go.opentelemetry.io/otel/metric v0.18.0 // indirect
-	go.opentelemetry.io/otel/sdk/export/metric v0.18.0 // indirect
-	go.opentelemetry.io/otel/trace v0.18.0 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.7.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.7.0 // indirect
+	go.opentelemetry.io/otel/metric v0.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.16.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20211118181313-81c1377c94b1 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
