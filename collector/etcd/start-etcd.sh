b6edb39f3d4ac9b097a7c1dac0ae5083bf639f0b
Ryan Vacek <ryvace@protonmail.com>
2022-07-20 12:50:37 -0600

    Working etcd cluster w/ metrics endpoints at each region

--- collector/etcd/start-etcd.sh
+++ collector/etcd/start-etcd.sh
@@ -1,16 +1,23 @@
 #!/bin/zsh
 
-if [[ -z ETCD_CONFIG ]]; then
-    echo "running with custom config"
-    sed -i "s/etcd-name/${ETCD_NODE_NAME}/g" ${ETCD_CONFIG}
-    /usr/local/bin/etcd --config-file ${ETCD_CONFIG}
-    exit 0
-    echo "running with default config"
-    /usr/local/bin/etcd
-    exit 0
-fi
+set -e -x
+
+FLAGS=${@}
+
+FLAGS=$(echo -n "${FLAGS} \n
+    --name=${NAME} \n
+    --initial-advertise-peer-urls=http://${NAME}:2380 \n
+    --listen-peer-urls=http://0.0.0.0:2380 \n
+    --listen-client-urls=http://0.0.0.0:2379 \n
+    --advertise-client-urls=http://${NAME}:2379 \n
+    --heartbeat-interval=250 \n
+    --election-timeout=1250 \n
+    --initial-cluster-state=new \n
+    --listen-metrics-urls=http://0.0.0.0:5050"
+)
 
 
-echo "running with custom config"
-sed -i "s/etcd-name/${ETCD_NODE_NAME}/g" ${ETCD_CONFIG}
-/usr/local/bin/etcd --config-file ${ETCD_CONFIG}
+# --config-file=/etc/etcd.conf"
+
+echo -n "Running etcd ${FLAGS}"
+/usr/local/bin/etcd $(echo -n ${FLAGS})
