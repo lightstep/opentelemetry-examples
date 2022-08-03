209dc2b07120d84ef03586d7fcad31b7a7cdfde4
Heber Silva <heber.silva@lightstep.com>
2022-08-03 15:10:03 -0400

    Added Micrometer

---
+++ collector/prom-native-micrometer/micrometer-demo/src/test/java/com/example/metricsdemo/MetricsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micrometerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicriometerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
