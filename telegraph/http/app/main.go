790b11a328d12db0b188861a4df0584221124cb6
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2022-09-06 11:52:34 -0500

    telegraf: final conf http

--- telegraph/http/app/main.go
+++ telegraph/http/app/main.go
@@ -2,43 +2,68 @@ package main
 
 import (
 	"encoding/json"
+    "log"
 	"net/http"
+    "os"
 	"runtime"
 	"time"
 )
 
-type HeapStat struct {
-	Name  string `json:"name"`
-	Value uint64 `json:"value"`
-	Time  string `json:"time"`
+var (
+    hostname = "unset"
+    metricsPath = "/heapbasics"
+)
+
+func init() {
+    h, err := os.Hostname()
+    if err != nil {
+        hostname = "undetected"
+    }
+    hostname = h
+}
+
+type Fields struct {
+    Idle uint64 `json:"idle"`
+    Inuse uint64 `json:"inuse"`
+    Reserved uint64 `json:"reserved"`
+}
+
+type Tags struct {
+    Host string `json:"hostname"`
+    MetricsPath string `json:"metrics_path"`
+}
+
+type HeapStats struct {
+    Name string `json:"name"`
+    Fields Fields `json:"fields"`
+    Tags Tags `json:"tags"`
+	Timestamp  int64 `json:"timestamp"`
 }
 
 func main() {
-	http.HandleFunc("/heapbasics", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(metricsPath, func(w http.ResponseWriter, r *http.Request) {
+        log.Println("metrics requested")
 		var ms runtime.MemStats
 		runtime.ReadMemStats(&ms)
 
-		heapMetrics := []*HeapStat{
-			{
-				Name:  "idle",
-				Value: ms.HeapIdle,
-				Time:  time.Now().Format("unix"),
-			},
-			{
-				Name:  "inuse",
-				Value: ms.HeapInuse,
-				Time:  time.Now().Format("unix"),
-			},
-			{
-				Name:  "reserved",
-				Value: ms.HeapSys,
-				Time:  time.Now().Format("unix"),
-			},
+		heapMetrics := HeapStats{
+            Name: "heapstats",
+            Fields: Fields{
+                Idle: ms.HeapIdle,
+                Inuse: ms.HeapInuse,
+                Reserved: ms.HeapSys,
+            },
+            Tags: Tags{
+                Host: hostname,
+                MetricsPath: metricsPath, 
+            },
+            Timestamp: time.Now().Unix(),
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(heapMetrics)
+        log.Println("metrics provided")
 	})
 
 	http.ListenAndServe(":8080", nil)
