8baae13475be3d17598b954d3ba7884580be6d7d
Heber Silva <heber.silva@lightstep.com>
2023-05-03 13:58:12 -0400

    modified gitlab config

--- collector/prom-native-gitlab/srv/gitlab/config/gitlab.rb
+++
@@ -1,9 +0,0 @@
-# Enable HTTP for the Prometheus endpoint
-prometheus['enable'] = true
-prometheus['listen_address'] = 'localhost:9090'
-prometheus['ssl_enabled'] = false
-
-# Allow Prometheus to access the metrics endpoint
-gitlab_workhorse['prometheus_listen_addr'] = "localhost:9229"
-
-external_url 'http://gitlab.example.com'
